0ebaccfdb8ea2f50139e00d770ea566ab2da5987
piyushtripathi0602 <piyushtripathi0602@gmail.com>
2022-11-14 12:36:11 +0530

    My First Comit

---
+++ Komal/src/com/velocity/Demo.java
@@ -0,0 +1,10 @@
+package com.velocity;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Hiii");
+
+	}
+
+}
---
+++ Komal/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author piyus
+ *
+ */
+module Komal {
+}
